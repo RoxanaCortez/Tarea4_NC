30e54cf62fdadeaa6737f15df1d0b7d448a6f93c
RoxanaCortez <00502211@uca.edu.sv>
2020-05-02 18:56:50 -0600

    Anotaciones para validaciones agregadas

--- tarea4/src/main/java/com/uca/capas/tarea4/domain/Producto.java
+++ tarea4/src/main/java/com/uca/capas/tarea4/domain/Producto.java
@@ -1,18 +1,36 @@
 package com.uca.capas.tarea4.domain;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class Producto {
+	//Usando anotaciondes de hibernate para validaciones
+	@Pattern(regexp="\\d{12}", message = "El campo Código de Producto Universal debe tener exactamente 12 dígitos.")
 	private String codigo;
+	
+	@Size(min = 1, max = 10, message = "El campo Nombre Producto debe tener una longitud entre 1 y 100 caracteres.")
 	private String nombre;
+	
+	@Size(min = 1, max = 10, message = "El campo Marca debe tener una longitud entre 1 y 100 caracteres.")
 	private String marca;
+	
+	@Size(min = 1, max = 5, message = "El campo Descripción debe tener una longitud entre 1 y 500 caracteres.")
 	private String descripcion;
+	
+	@Digits(integer = 5, fraction = 0, message = "El campo Existencias no puede tener números con decimales")
 	private String existencias;
+	
+	@Pattern(regexp = "^([0-2][0-9]|(3)[0-1])(\\/)(((0)[0-9])|((1)[0-2]))(\\/)\\d{4}$", 
+			 message = "El campo Fecha de Ingreso debe estar en formato dd/mm/yyyy")
 	private String fIngreso;
 	
+	//Constructor 
 	public Producto() {
 		
 	}
 	
-
+	//Constructor
 	public Producto(String codigo, String nombre, String marca, String descripcion, String existencias,
 			String fIngreso) {
 		super();
@@ -24,7 +42,7 @@ public class Producto {
 		this.fIngreso = fIngreso;
 	}
 
-
+	//Getters y Setters
 	public String getCodigo() {
 		return codigo;
 	}
